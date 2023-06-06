2b6467b237173c105285973c7eeeee0c8818b303
Joao-otavio04 <135053914+Joao-otavio04@users.noreply.github.com>
2023-06-06 15:00:31 -0300

    Update comparar.py

--- comparar.py
+++ comparar.py
@@ -3,9 +3,15 @@ def comparar(enig, compa):
     porc_eng = list(enig.values())
     letra_por = list(compa.keys())
     porc_por = list(compa.values())
-    delta = []
+    resultado_lista = []
+    resultado_tupla = {}
+    
     for eng in porc_eng:
+        delta = []
         for prob in porc_por:
             delta.append(abs(eng-prob))
         index = delta.index(min(delta))
-        print(delta[index])
+        resultado_lista.append(letra_por[index])
+    for n, c in enumerate(letra_eng):
+        resultado_tupla[c] = resultado_lista[n]
+    return resultado_tupla
